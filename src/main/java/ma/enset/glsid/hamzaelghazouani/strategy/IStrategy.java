da1378d71281b7034c380ed70495593851673d0a
hamzaelghazouani1 <elghazouani.hamza1@gmail.com>
2025-11-02 10:00:00 +0100

    Maven Config and Interface

---
+++ src/main/java/ma/enset/glsid/hamzaelghazouani/strategy/IStrategy.java
@@ -0,0 +1,12 @@
+package ma.enset.glsid.hamzaelghazouani.strategy;
+
+/**
+ * Interface defining the strategy contract.
+ * Part of the Strategy Design Pattern.
+ */
+public interface IStrategy {
+    /**
+     * Executes the strategy algorithm.
+     */
+    void appliquer();
+}
